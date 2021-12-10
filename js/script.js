622d5cbcdb6976c77202a175424a44c358f0cc83
Hubert Dyrda <flovlie.pro@gmail.com>
2021-12-10 16:31:18 +0100

    whole currency converter modernized

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
-let sumElement = document.querySelector(".js-sum");
-let exchangeRateElement = document.querySelector(".js-exchangeRate");
+let amountElement = document.querySelector(".js-amount");
+let currencyElement = document.querySelector(".js-currency");
 let resultElement = document.querySelector(".js-result");
 let formElement = document.querySelector(".js-form");
+
+let rateEUR = 4.62;
+let rateGBP = 5.40;
+let rateUSD = 4.09;
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let sum = sumElement.value;
-    let exchangeRate = exchangeRateElement.value;
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
 
-    let result = sum * exchangeRate;
+    let result;
 
-    resultElement.innerText = result;
+    switch (currency) {
+        case "EUR":
+            result = amount / rateEUR;
+            break;
 
+        case "GBP":
+            result = amount / rateGBP;
+            break;
+
+        case "USD":
+            result = amount / rateUSD;
+            break;
+    }
+  resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency} </strong>`;
 });
-
 
 
